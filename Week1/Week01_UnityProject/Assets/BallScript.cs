4ff2566574e609e4ca52bcf415ec8465de29fcb0
Alexander King <alexander@Akingsquest.com>
2018-09-01 15:30:01 -0400

    adding game from class

---
+++ Week1/Week01_UnityProject/Assets/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+
+	Vector3 initialForce;
+
+	// Use this for initialization
+	void Start () {
+		initialForce.x = Random.Range(-5000f, 5000f);
+		initialForce.z = Random.Range(-5000f, 5000f);
+
+		GetComponent<Rigidbody>().AddForce(initialForce);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	
+}
---
+++ Week1/Week01_UnityProject/Assets/BallSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	float spawnTimer = 0;
+	void Update () {
+		spawnTimer += 1f;
+		if (spawnTimer > 120f) {
+			GameObject.Instantiate(ballToSpawn);
+			spawnTimer = 0;
+		}
+	}
+}
