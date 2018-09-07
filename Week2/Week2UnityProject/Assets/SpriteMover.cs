86f481e4d3929425ce720ef9118da71d89c39f3a
Alexander King <alexander@Akingsquest.com>
2018-09-07 15:39:12 -0400

    Add files via upload

---
+++ Week2/Week2UnityProject/Assets/SpriteMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour {
+
+	public float moveDistance;
+	public float moveSpeed;
+	public Vector3 newPosition;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newPosition.y = moveDistance * Mathf.Sin(moveSpeed * Time.time);
+		this.transform.position = newPosition;
+	}
+}
---
+++ Week2/Week2UnityProject/Assets/SpriteRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotator : MonoBehaviour {
+
+	public float moveDistance;
+	public float moveSpeed;
+	public Vector3 newPosition;
+
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		newPosition.z = moveDistance * Mathf.Sin(moveSpeed * Time.time);
+		this.transform.rotation = Quaternion.Euler(newPosition);
+	}
+}
