5ec511e46c559e29ecd20aa85d9bc1b69ffc8095
Alexander King <alexander@Akingsquest.com>
2018-10-18 11:39:59 -0400

    Add files via upload

---
+++ Week08/Capture/Assets/FrameCapture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCapture : MonoBehaviour {
+
+	public string folderName = "Screenshots";
+	public int frameRate = 30;
+	private int frameNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		System.IO.Directory.CreateDirectory(folderName);
+		Time.captureFramerate = frameRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string fileName = string.Format("{0}/frame{1:D04}.png", folderName, frameNum);
+		Debug.Log(fileName);
+		ScreenCapture.CaptureScreenshot(fileName);
+		frameNum++;
+	}
+}
---
+++ Week08/Capture/Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 rotationVector;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(rotationVector * Time.deltaTime);
+	}
+}
