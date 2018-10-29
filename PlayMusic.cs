257cdf9cf2aac610f36ee190bb5e7f8f96894ce5
Alexander King <alexander@Akingsquest.com>
2018-10-29 13:41:46 -0400

    Add files via upload

---
+++ PlayMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour {
+    AudioSource myaudio;
+
+	// Use this for initialization
+	void Start () {
+        myaudio = this.gameObject.GetComponent<AudioSource>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            
+            myaudio.Play();
+
+            myaudio.pitch = Random.Range(.7f, 1.4f);
+        }
+	}
+}
